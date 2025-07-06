71f01ed72cf8e8d4da1de1f32777378bfb1206b1
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-06 16:16:58 +0330

    Update HomeHeaderSection component : create data file and tags ...

--- src/components/pages/HomePage/HomePageCom/HomeHeaderSection/HomeHeaderSection.jsx
+++ src/components/pages/HomePage/HomePageCom/HomeHeaderSection/HomeHeaderSection.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import './HomeHeaderSection.css'
+import React from "react";
+import "./HomeHeaderSection.css";
+import HomeHeaderSectionData from "./HomeHeaderSectionData";
 
 export default function HomeHeaderSection() {
   return (
-    <div>HomeHeaderSection</div>
-  )
+    <div className="home-header-bg">
+      <section className="home-header-section">
+        {HomeHeaderSectionData.map((item) => (
+          <div className="home-header-section-box" key={item.id}>
+            <div className="header-left-side">
+                <h3 className="header-left-sub-title">{item.header}</h3>
+                <h1 className="header-left-title">{item.title}</h1>
+                <p className="header-left-paragraph">{item.description}</p>
+            </div>
+            <img className="header-img" src={item.image} alt="fastfood image"/>
+            <div className="header-button"></div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
--- src/components/pages/HomePage/HomePageCom/HomeHeaderSection/HomeHeaderSectionData.js
+++ src/components/pages/HomePage/HomePageCom/HomeHeaderSection/HomeHeaderSectionData.js
@@ -5,5 +5,9 @@ const HomeHeaderSectionData = [
     title: "Your Go-To Spot for Quick and Tasty Eats!",
     description:
       "Where quick eats and quality collide, crafting a taste sensation. Speed meets flavor in every bite, promising a culinary journey that's as swift as it is delicious.",
+    image:
+      "/image/661cbfbcac04478f46ca34b9_78178740_Fresh beef burger isolated -1 2.png",
   },
 ];
+
+export default HomeHeaderSectionData;
