d89c0cd34c7fcd94c2952e98cf17576e45074a58
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-07 11:54:07 +0330

    Create SwiperSliderSection component...

---
+++ src/components/pages/HomePage/HomePageCom/SwiperSliderSection/SwiperSliderSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import SwiperSliderdata from './SwiperSliderdata'
+import './SwiperSliderSection.css'
+
+export default function SwiperSliderSection() {
+  return (
+    <div>SwiperSliderSection</div>
+  )
+}
---
+++ src/components/pages/HomePage/HomePageCom/SwiperSliderSection/SwiperSliderdata.js
@@ -0,0 +1,5 @@
+const SwiperSliderdata = [
+    
+];
+
+export default SwiperSliderdata;
