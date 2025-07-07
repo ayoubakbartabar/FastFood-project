441636d8ebdafa7fdc541c35fc7ebd985f5dad0c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-07 12:41:26 +0330

    Update SwiperSliderSection.css : set styles...

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -3,12 +3,14 @@ import "./HomePage.css";
 import Navbar from "../../shared/Navbar/Navbar.jsx";
 import Footer from "../../shared/Footer/Footer.jsx";
 import HomeHeaderSection from "./HomePageCom/HomeHeaderSection/HomeHeaderSection.jsx";
+import SwiperSliderSection from "./HomePageCom/SwiperSliderSection/SwiperSliderSection.jsx";
 
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <HomeHeaderSection />
+      <SwiperSliderSection/>
       <Footer />
     </>
   );
--- src/components/pages/HomePage/HomePageCom/SwiperSliderSection/SwiperSliderdata.js
+++ src/components/pages/HomePage/HomePageCom/SwiperSliderSection/SwiperSliderdata.js
@@ -1,5 +1,52 @@
-const SwiperSliderdata = [
-    
+const SwiperSliderData = [
+  {
+    id: 1,
+    image: "/image/661cbcd37d541eea8b29644a_combo icon.png",
+    title: "Combo Meals",
+    href: "#",
+  },
+  {
+    id: 2,
+    image: "/image/661cbc77533d0a6b182c9a5b_pasta icon.png",
+    title: "Pasta",
+    href: "#",
+  },
+  {
+    id: 3,
+    image: "/image/661cbc66491ead7e40f8ba4e_pizza icon.png",
+    title: "Pizza",
+    href: "#",
+  },
+  {
+    id: 4,
+    image: "/image/661cbc4f97149635cdf9dbe4_burger icon.png",
+    title: "Burger",
+    href: "#",
+  },
+  {
+    id: 5,
+    image: "/image/661cbc8d8cb1484ef3056129_sandwich icon.png",
+    title: "Sandwich",
+    href: "#",
+  },
+  {
+    id: 6,
+    image: "/image/661cbc998b5bdd7f338f485a_drinks icon.png",
+    title: "Drinks",
+    href: "#",
+  },
+  {
+    id: 7,
+    image: "/image/661cbcb9533d0a6b182cee2d_chicken-icon.png",
+    title: "Sides & Fries",
+    href: "#",
+  },
+  {
+    id: 8,
+    image: "/image/661cbcb5dd419285fc7219d1_meal icon.png",
+    title: "Kids' Meals",
+    href: "#",
+  },
 ];
 
-export default SwiperSliderdata;
+export default SwiperSliderData;
