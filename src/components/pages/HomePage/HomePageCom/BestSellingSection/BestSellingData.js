d38db661e93d7e6ae51e8b4b98ad7e1e1783899a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-08 10:59:18 +0330

    Update BestSellingData.js : create properties...

--- src/components/pages/HomePage/HomePageCom/BestSellingSection/BestSellingData.js
+++ src/components/pages/HomePage/HomePageCom/BestSellingSection/BestSellingData.js
@@ -1,3 +1,62 @@
-const BestSellingData = [];
+const BestSellingData = [
+  {
+    id: 1,
+    image: "/image/66210efa8e4b4099fd114212_pasta image.png",
+    title: "Pasta with Tomatoes and Basil",
+    star: 5,
+    price: 30.0,
+  },
+  {
+    id: 2,
+    image:
+      "/image/661cbfbcac04478f46ca34b9_78178740_Fresh beef burger isolated -1 2.png",
+    title: "Testy Beef Burger",
+    star: 5,
+    price: 41.0,
+  },
+  {
+    id: 3,
+    image: "/image/661cc038dcc14c75443911c3_image.png",
+    title: "Spinach Artichoke Spectacle",
+    star: 5,
+    price: 48.0,
+  },
+  {
+    id: 4,
+    image: "/image/661cc01f38e41da22474a52a_fast_food_set 1.png",
+    title: "TNC Combo Meals",
+    star: 4.5,
+    price: 41.0,
+  },
+  {
+    id: 5,
+    image:
+      "/image/661cc047c3a82b8be8890aa0_wepik-export-20240119111930TbUs 1.png",
+    title: "Tasty Mint Julep Cocktail",
+    star: 3.5,
+    price: 75.0,
+  },
+  {
+    id: 6,
+    image: "/image/661cc009926e82c8f0e27899_Tasty mint julep cocktail 1.png",
+    title: "Lemon Ice Tea",
+    star: 5,
+    price: 15.0,
+  },
+  {
+    id: 7,
+    image: "/image/66210df4c3b3464a6a16c59c_image.png",
+    title: "Fried Chicken Legs",
+    star: 5,
+    price: 20.0,
+  },
+  {
+    id: 8,
+    image: "/image/661cbf8e0563fed591d090fd_image-2.png",
+    title: "Philly Steak Sandwich",
+    star: 4.5,
+    price: 35.0,
+  },
+];
 
 export default BestSellingData;
