38302b2935830467f0e126246bf8b0f76cfb98f1
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-13 17:49:55 +0330

    Update HomePageBlogData.js  : create properties...

---
+++ src/components/pages/HomePage/HomePageCom/HomePageBlogSection/HomePageBlogData.js
@@ -0,0 +1,28 @@
+const HomePageBlogData = [
+  {
+    id: 1,
+    image: "/image/blog-image(1).png",
+    category: "Subway",
+    title: "The Inspiring Fusion of Art and Cuisine",
+    paragraph:
+      "Exploring the evolving landscape of FastFood TNC the blog delves into its impact on consumer habits and the broader food",
+  },
+  {
+    id: 2,
+    image: "/image/blog-image(2).png",
+    category: "Italian Dish",
+    title: "Pairing the Perfect Wines with TNC's Delightful Dishes",
+    paragraph:
+      "Exploring the evolving landscape of FastFood TNC the blog delves into its impact on consumer habits and the broader food",
+  },
+  {
+    id: 3,
+    image: "/image/blog-image(3).png",
+    category: "Subway",
+    title: "A Culinary Journey Through TNC's Signature Dishes",
+    paragraph:
+      "Exploring the evolving landscape of FastFood TNC the blog delves into its impact on consumer habits and the broader food",
+  },
+];
+
+export default HomePageBlogData;
