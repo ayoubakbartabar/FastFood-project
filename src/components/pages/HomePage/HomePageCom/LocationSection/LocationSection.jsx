85b3c0600db9c7d57f9bfc811fc1f915074f13be
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-14 18:44:15 +0330

    Update LocationSection.jsx and styles : create tags and set styles ...

--- src/components/pages/HomePage/HomePageCom/LocationSection/LocationSection.jsx
+++ src/components/pages/HomePage/HomePageCom/LocationSection/LocationSection.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import './LocationSection.css'
+import React from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import "./LocationSection.css";
+
+const containerStyle = {
+  width: "100%",
+  height: "500px",
+};
+
+const center = {
+  lat: 40.6782,
+  lng: -73.775,
+};
 
 export default function LocationSection() {
   return (
-    <section className='location-section'>LocationSection</section>
-  )
+    <section className="location-section">
+      <LoadScript googleMapsApiKey="AIzaSyC7Quoik8cWwBpZDwqyED5ctTc5NEKSqLk">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
+    </section>
+  );
 }
