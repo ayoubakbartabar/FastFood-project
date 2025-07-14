68bd8a7b1b92084a20534449e967785016a691a8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-14 19:07:00 +0330

    Update SocialSection.jsx , styles and data file : create tags , set styles and properties ...

--- src/components/pages/HomePage/SocialSection/SocialData.js
+++ src/components/pages/HomePage/SocialSection/SocialData.js
@@ -1,3 +1,30 @@
-const SocialData = [];
+const SocialData = [
+  {
+    id: 1,
+    image: "/image/Social(1).webp",
+    alt: "product-image",
+  },
+  {
+    id: 2,
+    image: "/image/661cabff491ead7e40ea570d_Rectangle 23.webp",
+    alt: "product-image",
+  },
+  {
+    id: 3,
+    image:
+      "/image/661cabff491ead7e40ea570e_wepik-export-20240101084202rP9g 1.webp",
+    alt: "product-image",
+  },
+  {
+    id: 4,
+    image: "/image/661cabff491ead7e40ea5717_Rectangle 19.webp",
+    alt: "product-image",
+  },
+  {
+    id: 5,
+    image: "/image/661cabff491ead7e40ea5733_Rectangle 22.webp",
+    alt: "product-image",
+  },
+];
 
 export default SocialData;
--- src/components/pages/HomePage/SocialSection/SocialSection.jsx
+++ src/components/pages/HomePage/SocialSection/SocialSection.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import "./SocialSection.css";
+import { FaInstagram } from "react-icons/fa";
 import SocialData from "./SocialData";
+import "./SocialSection.css";
 
 export default function SocialSection() {
   return (
     <div className="social-bg">
-      <section className="social-section">SocialSection</section>
+      <section className="social-section">
+        {SocialData.map(({ id, image, alt }) => (
+          <figure key={id} className="social-image">
+            <img src={image} alt={alt} loading="lazy" />
+          </figure>
+        ))}
+
+        <a
+          href="#"
+          className="social-icon"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+      </section>
     </div>
   );
 }
