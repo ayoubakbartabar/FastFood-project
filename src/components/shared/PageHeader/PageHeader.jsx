3f2f2bac81be08a32891f05505d3a48a6594b39b
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-16 18:09:24 +0330

    Create PageHeader component...

---
+++ src/components/shared/PageHeader/PageHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "/PageHeader.css";
+
+export default function PageHeader() {
+  return (
+    <div className="page-header-bg">
+      <section className="page-header-section"></section>
+    </div>
+  );
+}
