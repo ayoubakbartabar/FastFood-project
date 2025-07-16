e75a132d0b69f137302050b71fab16f3c7f991df
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-16 18:25:53 +0330

    Create BrandIntroSection component...

--- src/components/pages/AboutPage/AboutPage.jsx
+++ src/components/pages/AboutPage/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./AboutPage.css";
 import Navbar from "../../shared/Navbar/Navbar";
 import PageHeader from "../../shared/PageHeader/PageHeader";
+import BrandIntroSection from "./AboutPageCom/BrandIntroSection/BrandIntroSection";
 
 export default function AboutPage() {
   return (
     <>
       <Navbar />
       <PageHeader title={"About us"} />
+      <BrandIntroSection />
     </>
   );
 }
---
+++ src/components/pages/AboutPage/AboutPageCom/BrandIntroSection/BrandIntroSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BrandIntroSection.css";
+
+export default function BrandIntroSection() {
+  return (
+    <div className="brand-intro-bg">
+      <section className="brand-intro-section">brand-intro</section>
+    </div>
+  );
+}
