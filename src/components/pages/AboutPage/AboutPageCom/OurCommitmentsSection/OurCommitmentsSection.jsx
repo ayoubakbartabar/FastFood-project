2cf30e554038d51ac7e61a711e5e6ec66b745148
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-18 14:37:27 +0330

    Update OurCommitmentsSection.jsx : create tags....

--- src/components/pages/AboutPage/AboutPageCom/OurCommitmentsSection/OurCommitmentsSection.jsx
+++ src/components/pages/AboutPage/AboutPageCom/OurCommitmentsSection/OurCommitmentsSection.jsx
@@ -1,3 +1,4 @@
+// OurCommitmentsSection.js
 import React from "react";
 import "./OurCommitmentsSection.css";
 import OurCommitmentsData from "./OurCommitmentsData.js";
@@ -6,7 +7,27 @@ export default function OurCommitmentsSection() {
   return (
     <div className="our-commitments-bg">
       <section className="our-commitments-section">
-        OurCommitmentsSection
+        <div className="our-commitments-top">
+          <h1 className="our-commitments-title">Our Commitments</h1>
+          <p className="our-commitments-paragraph">
+            Invite edit component vertical rectangle component follower asset
+            share. Main select community connection opacity share device.
+          </p>
+        </div>
+
+        <div className="our-commitments-grid">
+          {OurCommitmentsData.map((item) => (
+            <div key={item.id} className="our-commitments-box">
+              <img
+                className="our-commitments-box-image"
+                src={item.image}
+                alt={item.title}
+              />
+              <h2 className="our-commitments-box-title">{item.title}</h2>
+              <p className="our-commitments-box-paragraph">{item.paragraph}</p>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
