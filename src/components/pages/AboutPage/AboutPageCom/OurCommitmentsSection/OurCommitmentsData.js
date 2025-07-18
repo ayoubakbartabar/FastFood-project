c9828eb09d4e986c8f65f1bb4855a3ab0a09f76d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-18 14:19:49 +0330

    Create OurCommitmentsSection component ...

---
+++ src/components/pages/AboutPage/AboutPageCom/OurCommitmentsSection/OurCommitmentsData.js
@@ -0,0 +1,3 @@
+const OurCommitmentsData = [];
+
+export default OurCommitmentsData;
---
+++ src/components/pages/AboutPage/AboutPageCom/OurCommitmentsSection/OurCommitmentsSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./OurCommitmentsSection.css";
+import OurCommitmentsData from "./OurCommitmentsData.js";
+
+export default function OurCommitmentsSection() {
+  return (
+    <div className="our-commitments-bg">
+      <section className="our-commitments-section">
+        OurCommitmentsSection
+      </section>
+    </div>
+  );
+}
