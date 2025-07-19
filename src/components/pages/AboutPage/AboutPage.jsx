5c7ea124a13b4696d568030ed9ecfbee1949dda8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-19 09:02:07 +0330

    Update QuickStatsSection.css : set styles...

--- src/components/pages/AboutPage/AboutPage.jsx
+++ src/components/pages/AboutPage/AboutPage.jsx
@@ -5,6 +5,7 @@ import PageHeader from "../../shared/PageHeader/PageHeader";
 import BrandIntroSection from "./AboutPageCom/BrandIntroSection/BrandIntroSection";
 import VideoSection from "./AboutPageCom/VideoSection/VideoSection";
 import OurCommitmentsSection from "./AboutPageCom/OurCommitmentsSection/OurCommitmentsSection";
+import QuickStatsSection from "./AboutPageCom/QuickStatsSection/QuickStatsSection";
 
 export default function AboutPage() {
   return (
@@ -14,6 +15,7 @@ export default function AboutPage() {
       <BrandIntroSection />
       <VideoSection />
       <OurCommitmentsSection />
+      <QuickStatsSection />
     </>
   );
 }
