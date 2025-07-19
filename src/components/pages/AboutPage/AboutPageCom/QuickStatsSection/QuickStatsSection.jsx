78f7ef25489bcd1ed9f5284fd883d4068596a058
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-19 08:44:12 +0330

    Create QuickStatsSection component ...

---
+++ src/components/pages/AboutPage/AboutPageCom/QuickStatsSection/QuickStatsSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './QuickStatsSection.css'
+
+export default function QuickStatsSection() {
+  return (
+    <div className='quick-stats-bg'>
+        <section className="quick-stats-section">
+            
+        </section>
+    </div>
+  )
+}
