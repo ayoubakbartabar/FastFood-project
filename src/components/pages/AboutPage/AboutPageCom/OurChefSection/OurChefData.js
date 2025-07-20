1799f117a95eb87e376ef3603536832c36e2115f
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-20 16:32:20 +0330

    Update OurChefData and image file: create properties and add new image...

--- src/components/pages/AboutPage/AboutPageCom/OurChefSection/OurChefData.js
+++ src/components/pages/AboutPage/AboutPageCom/OurChefSection/OurChefData.js
@@ -1,3 +1,55 @@
-const OurChefData = [];
+const OurChefData = [
+  {
+    id: 1,
+    name: "Brooklyn Simmons",
+    role: "Pizza Specialist",
+    image:
+      "/image/659e6d050a2af686af339d58_wepik-export-20240104041528hu14 1.png",
+  },
+  {
+    id: 2,
+    name: "Jane Cooper",
+    role: "Burger Specialist",
+    image:
+      "/image/661e105aa3542c723fa34577_wepik-export-20240102125523pipv 1.webp",
+  },
+  {
+    id: 3,
+    name: "Jerome Bell",
+    role: "Head Chef",
+    image: "/image/661e154024524e66c1a62274_image.png",
+  },
+  {
+    id: 4,
+    name: "Courtney Henry",
+    role: "Pasta Specialist",
+    image:
+      "/image/661e105aa3542c723fa34572_wepik-export-20240103130957IaVi%201-p-500.webp",
+  },
+  {
+    id: 5,
+    name: "Brooklyn Simmons",
+    role: "Pizza Specialist",
+    image: "/image/661e153fc80ef94d0d6a22d8_image-7.png",
+  },
+  {
+    id: 6,
+    name: "Jane Cooper",
+    role: "Burger Specialist",
+    image: "/image/661e153f03768e836598f9e5_image-3.png",
+  },
+  {
+    id: 7,
+    name: "Jerome Bell",
+    role: "Head Chef",
+    image: "/image/661e153f9543d43b5e7fe74d_imaage.png",
+  },
+  {
+    id: 8,
+    name: "Mark John",
+    role: "Cuisine Specialist",
+    image: "/image/661e153fd040d5c09a81a088_image-4.png",
+  },
+];
 
 export default OurChefData;
