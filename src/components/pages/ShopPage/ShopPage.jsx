7674fb5eafa1b059e18001d3625070aa7e9f1e0e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-23 18:52:40 +0330

    Update ShopPage.jsx : add Footer and socialSection ....

--- src/components/pages/ShopPage/ShopPage.jsx
+++ src/components/pages/ShopPage/ShopPage.jsx
@@ -3,12 +3,16 @@ import "./ShopPage.css";
 import Navbar from "../../shared/Navbar/Navbar.jsx";
 import PageHeader from "../../shared/PageHeader/PageHeader.jsx";
 import ShopSliderSection from "./ShopPageCom/ShopSliderSection/ShopSliderSection.jsx";
+import SocialSection from "../../shared/SocialSection/SocialSection.jsx";
+import Footer from "../../shared/Footer/Footer.jsx";
 export default function ShopPage() {
   return (
     <>
       <Navbar />
       <PageHeader title={"Shop"} />
       <ShopSliderSection />
+      <SocialSection />
+      <Footer />
     </>
   );
 }
