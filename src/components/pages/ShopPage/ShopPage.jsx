5a3dbb17dd3cf0eb6cee958f260e0abafe2fb595
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-23 18:50:55 +0330

    Update ShopPageCom : create ShopSliderSection components..

--- src/components/pages/ShopPage/ShopPage.jsx
+++ src/components/pages/ShopPage/ShopPage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import './ShopPage.css'
-import Navbar from '../../shared/Navbar/Navbar.jsx'
-
+import React from "react";
+import "./ShopPage.css";
+import Navbar from "../../shared/Navbar/Navbar.jsx";
+import PageHeader from "../../shared/PageHeader/PageHeader.jsx";
+import ShopSliderSection from "./ShopPageCom/ShopSliderSection/ShopSliderSection.jsx";
 export default function ShopPage() {
   return (
-    <div>
-      <Navbar/>
-    </div>
-  )
+    <>
+      <Navbar />
+      <PageHeader title={"Shop"} />
+      <ShopSliderSection />
+    </>
+  );
 }
---
+++ src/components/pages/ShopPage/ShopPageCom/ShopSliderSection/ShopSliderSection.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ShopSliderSection.css'
+
+export default function ShopSliderSection() {
+  return (
+    <div>ShopSliderSection</div>
+  )
+}
