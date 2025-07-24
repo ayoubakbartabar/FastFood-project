550d1343dc4c6a08243499774623f976d2e64b67
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-24 08:21:52 +0330

    Update ShopPage.jsx and sytles: create ne template and set styles..

--- src/components/pages/ShopPage/ShopPage.jsx
+++ src/components/pages/ShopPage/ShopPage.jsx
@@ -3,16 +3,21 @@ import "./ShopPage.css";
 import Navbar from "../../shared/Navbar/Navbar.jsx";
 import PageHeader from "../../shared/PageHeader/PageHeader.jsx";
 import ShopAsideSection from "./ShopPageCom/ShopAsideSection/ShopAsideSection.jsx";
+import ShopProductSection from "./ShopPageCom/ShopProductSection/ShopProductSection.jsx";
 import SocialSection from "../../shared/SocialSection/SocialSection.jsx";
 import Footer from "../../shared/Footer/Footer.jsx";
-import ShopProductSection from "./ShopPageCom/ShopProductSection/ShopProductSection.jsx";
+
 export default function ShopPage() {
   return (
     <>
       <Navbar />
       <PageHeader title={"Shop"} />
-      <ShopAsideSection />
-      <ShopProductSection />
+
+      {/* WRAPPER FLEX: Aside + Product */}
+      <div className="shop-main-wrapper">
+        <ShopAsideSection />
+        <ShopProductSection />
+      </div>
       <SocialSection />
       <Footer />
     </>
