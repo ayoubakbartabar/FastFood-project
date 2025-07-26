dbc00136da8d4a8aec994d3fd11f77acc6f6ea65
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-26 19:10:37 +0330

    Update ShopAsideSection.jsx : set new feature...

--- src/components/pages/ShopPage/ShopPageCom/ShopAsideSection/ShopAsideSection.jsx
+++ src/components/pages/ShopPage/ShopPageCom/ShopAsideSection/ShopAsideSection.jsx
@@ -3,13 +3,17 @@ import "./ShopAsideSection.css";
 import ShopProductData from "../ShopProductData/ShopProductData";
 import { FaSearch } from "react-icons/fa";
 
-export default function ShopAsideSection() {
-  // create const for set category
-  const newCat = [
+export default function ShopAsideSection({
+  selectedCategory,
+  setSelectedCategory,
+}) {
+  const categories = [
+    "all",
     ...new Set(
       ShopProductData.map((item) => item.category.trim().toLowerCase())
     ),
   ];
+
   return (
     <aside className="shop-aside">
       <div className="shop-search-box">
@@ -22,8 +26,14 @@ export default function ShopAsideSection() {
       <div className="shop-categories-box">
         <h3 className="shop-categories-title">Categories</h3>
         <ul className="shop-categories-list">
-          {newCat.map((category, index) => (
-            <li key={index} className="shop-category-item">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className={`shop-category-item ${
+                selectedCategory === category ? "active" : ""
+              }`}
+              onClick={() => setSelectedCategory(category)}
+            >
               {category}
             </li>
           ))}
