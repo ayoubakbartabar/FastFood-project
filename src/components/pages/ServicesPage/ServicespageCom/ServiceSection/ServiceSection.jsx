79f3b2de1b11fa770deb1c6734f0e47e1b44099d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-27 15:07:38 +0330

    Update ServiceSection.jsx : create tags...

--- src/components/pages/ServicesPage/ServicespageCom/ServiceSection/ServiceSection.jsx
+++ src/components/pages/ServicesPage/ServicespageCom/ServiceSection/ServiceSection.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './ServiceSection.css'
+import React from "react";
+import ServiceSectionData from "./ServiceSectionData";
+import "./ServiceSection.css";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 export default function ServiceSection() {
   return (
-    <div className='service-bg'>
-        <section className="service-section"></section>
+    <div className="service-bg">
+      <section className="service-section">
+        <div className="service-grid">
+          {ServiceSectionData.map((item) => (
+            <div className="service-card" key={item.id}>
+              <div className="service-icon">
+                <img src={item.image} alt={item.title} />
+              </div>
+              <h3 className="service-title">{item.title}</h3>
+              <p className="service-paragraph">{item.paragraph}</p>
+              <button className="service-button">
+                View Details <FaLongArrowAltRight />
+              </button>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
-  )
+  );
 }
