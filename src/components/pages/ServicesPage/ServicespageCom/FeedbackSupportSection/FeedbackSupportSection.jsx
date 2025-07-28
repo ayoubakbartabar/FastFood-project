edf2f1afb1d09ad7ce3ed6fe2d22b70697e2f049
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-28 15:45:25 +0330

    Add new images...

--- src/components/pages/ServicesPage/ServicespageCom/FeedbackSupportSection/FeedbackSupportSection.jsx
+++ src/components/pages/ServicesPage/ServicespageCom/FeedbackSupportSection/FeedbackSupportSection.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './FeedbackSupportSection.css'
+import React from "react";
+import "./FeedbackSupportSection.css";
 
 export default function FeedbackSupportSection() {
+  const imagesArray = [
+    { id: 1, image: "/image/6620ae10f9fb43094b9427e9_image 1-p-500.png" },
+    { id: 2, image: "/image/6620ae1075f767e21dad441e_image 2-p-500.png" },
+    { id: 3, image: "/image/6620ae10531cab63f6d441d6_image 3-p-500.png" },
+  ];
   return (
-    <div className='feed-back-bg'>
-        <section className='feed-back-section'></section>
+    <div className="feed-back-bg">
+      <section className="feed-back-section"></section>
     </div>
-  )
+  );
 }
