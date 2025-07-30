8e718602092ba27627d9d02b0c8439442aa87133
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-30 08:59:08 +0330

    Update CategoriesSection.jsx : create synatx and tags...

--- src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/BlogAsideSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/BlogAsideSection.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./BlogAsideSection.css";
 import PopularPostSection from "./PopularPostSection/PopularPostSection";
+import CategoriesSection from "./CategoriesSection/CategoriesSection";
 
 export default function BlogAsideSection() {
   return (
     <div className="blog-layout-container">
       <PopularPostSection />
+      <CategoriesSection />
     </div>
   );
 }
--- src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/CategoriesSection/CategoriesSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/CategoriesSection/CategoriesSection.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './CategoriesSection.css'
+import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+import "./CategoriesSection.css";
+import PopularPost from "../BlogAsideData";
+
+// Extract unique categories
+const uniqueCategories = [
+  ...new Set(PopularPost.map((post) => post.categories.trim())),
+];
 
 export default function CategoriesSection() {
   return (
-    <>
-        <aside className='categories-aside'></aside>    
-    </>
-  )
+    <aside className="categories-aside">
+      <h3 className="categories-title">Categories</h3>
+      <div className="categories-underline"></div>
+      <ul className="categories-list">
+        {uniqueCategories.map((category, index) => (
+          <li key={index} className="category-item">
+            <FiChevronRight className="category-icon" />
+            <span>{category}</span>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
