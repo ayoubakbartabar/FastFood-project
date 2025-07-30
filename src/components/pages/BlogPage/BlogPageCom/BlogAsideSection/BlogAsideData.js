aaae0f201de9c04692d3e2d951fd9162fa13d07c
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-07-30 08:46:50 +0330

    Update BlogAsideData.js : set new property..

--- src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/BlogAsideData.js
+++ src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/BlogAsideData.js
@@ -3,32 +3,38 @@ const PopularPost = [
     id: 1,
     image: "/image/blog-image(1).png",
     title: "The Inspiring Fusion of Art and Cuisine",
+    categories: "Subway",
   },
   {
     id: 2,
     image: "/image/blog-image(2).png",
     title: "Pairing the Perfect Wines with TNC's Delightful Dishes",
+    categories: "Italian Dish",
   },
   {
     id: 3,
     image: "/image/blog-image(3).png",
     title: "A Culinary Journey Through TNC's Signature Dishes",
+    categories: "Subway",
   },
   {
     id: 4,
     image: "/image/blog-image(4).png",
     title: "Meet the culinary masterminds behind TNC's kitchen",
+    categories: "Dinner DIsh",
   },
   {
     id: 5,
     image: "/image/blog-image(5).png",
     title:
       "Explore TNC's green initiatives in the kitchen for sustainable dining",
+    categories: "Italian Dish",
   },
   {
     id: 6,
     image: "/image/blog-image(6).png",
     title: "Embark on a sweet journey with TNC's delectable desserts",
+    categories: "FastFood TNC",
   },
 ];
 
