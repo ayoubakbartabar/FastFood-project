70f3814c15fb7c5f50d6a20f7a83d628e0c3fa3c
Ayoub <162346549+ayoubakbartabar@users.noreply.github.com>
2025-07-31 12:11:53 +0330

    Update TagsSection.jsx: create tags...

--- src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/TagsSection/TagsSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogAsideSection/TagsSection/TagsSection.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './TagsSection.css'
+import React from "react";
+import "./TagsSection.css";
+import PopularPost from "../BlogAsideData";
+
+const uniqueTags = [
+  ...new Set(PopularPost.flatMap((post) => post.tags.map((tag) => tag.trim()))),
+];
 
 export default function TagsSection() {
   return (
-    <>
-    <aside className='tags-aside'></aside>
-    </>
-  )
+    <aside className="tags-aside">
+      <h3 className="tags-title">Tags</h3>
+      
+
+      
+        {uniqueTags.map((tag, index) => (
+          <span key={index} className="tag-item">
+            {tag}
+          </span>
+        ))}
+      
+
+    </aside>
+  );
 }
