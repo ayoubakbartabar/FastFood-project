0c0d12ee7a8c9dbf079d09d73e00ff2664fea3b1
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-01 19:43:31 +0330

    Update BlogsContentSection.jsx and styles : create tags and styles...

--- src/components/pages/BlogPage/BlogPageCom/BlogsContentSection/BlogsContentSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogsContentSection/BlogsContentSection.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './BlogsContentSection.css'
-import PopularPost from '../BlogAsideSection/BlogAsideData'
+import React from "react";
+import "./BlogsContentSection.css";
+import PopularPost from "../BlogAsideSection/BlogAsideData";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
 
 export default function BlogsContentSection() {
   return (
-    <section className='blogs-content-section'>BlogsContentSection</section>
-  )
+    <section className="blog-content-section">
+      <div className="blog-content-grid">
+        {PopularPost.map((item) => (
+          <div className="blog-content-card" key={item.id}>
+            <img
+              src={item.image}
+              alt={item.title}
+              className="blog-content-image"
+            />
+            <div className="blog-content-content">
+              <span className="blog-content-category">{item.category}</span>
+              <h3 className="blog-content-title">{item.title}</h3>
+              <p className="blog-content-paragraph">{item.paragraph}</p>
+              <a href="#" className="read-more">
+                Read more <MdOutlineArrowRightAlt />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
