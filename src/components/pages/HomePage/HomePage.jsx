f8c120b4d0338e98d4f3bbf42b3e03f6bcba976e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-02 15:42:07 +0330

    Update MenuPage.jsx : add components...

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomePage.css";
 import Navbar from "../../shared/Navbar/Navbar.jsx";
 import HomeHeaderSection from "./HomePageCom/HomeHeaderSection/HomeHeaderSection.jsx";
-import SwiperSliderSection from "./HomePageCom/SwiperSliderSection/SwiperSliderSection.jsx";
+import SwiperSliderSection from "../../shared/SwiperSliderSection/SwiperSliderSection.jsx";
 import BestSellingSection from "./HomePageCom/BestSellingSection/BestSellingSection.jsx";
 import HomeBannerSection from "./HomePageCom/HomeBannerSection/HomeBannerSection.jsx";
 import PopularItemsSection from "./HomePageCom/PopularItemsSection/PopularItemsSection.jsx";
--- src/components/pages/MenuPage/MenuPage.jsx
+++ src/components/pages/MenuPage/MenuPage.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './MenuPage.css'
+import React from "react";
+import "./MenuPage.css";
+import Navbar from "../../shared/Navbar/Navbar";
+import Footer from "../../shared/Footer/Footer";
+import PageHeader from "../../shared/PageHeader/PageHeader";
+import SocialSection from "../../shared/SocialSection/SocialSection";
+import SwiperSliderSection from "../../shared/SwiperSliderSection/SwiperSliderSection";
 
 export default function MenuPage() {
   return (
     <>
-        
+      <Navbar />
+      <PageHeader title={"Menu"} />
+      <SwiperSliderSection />
+      <SocialSection />
+      <Footer />
     </>
-  )
+  );
 }
