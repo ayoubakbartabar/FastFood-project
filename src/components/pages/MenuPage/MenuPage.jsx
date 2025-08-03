f3cad6ade8a5869a2b735de35fea1f04ae12fcb8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-03 20:38:48 +0330

    Update MenuPage.jsx..

--- src/components/pages/MenuPage/MenuPage.jsx
+++ src/components/pages/MenuPage/MenuPage.jsx
@@ -5,6 +5,7 @@ import Footer from "../../shared/Footer/Footer";
 import PageHeader from "../../shared/PageHeader/PageHeader";
 import SocialSection from "../../shared/SocialSection/SocialSection";
 import SwiperSliderSection from "../../shared/SwiperSliderSection/SwiperSliderSection";
+import MenuListSection from "./MenuPageCom/MenuListSection/MenuListSection";
 
 export default function MenuPage() {
   return (
@@ -12,6 +13,7 @@ export default function MenuPage() {
       <Navbar />
       <PageHeader title={"Menu"} />
       <SwiperSliderSection />
+      <MenuListSection />
       <SocialSection />
       <Footer />
     </>
