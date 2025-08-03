418f8723cbbb203e250087d701d99f37a5779778
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-03 20:21:49 +0330

    Create MenuListSection component...

---
+++ src/components/pages/MenuPage/MenuPageCom/MenuListSection/MenuListSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './MenuListSection.css'
+import BestSellingData from '../../../HomePage/HomePageCom/BestSellingSection/BestSellingData'
+
+export default function MenuListSection() {
+  return (
+    <div className='menu-list-bg'>
+        <section className='menu-list-section'>MenuListSection</section>
+    </div>
+  )
+}
