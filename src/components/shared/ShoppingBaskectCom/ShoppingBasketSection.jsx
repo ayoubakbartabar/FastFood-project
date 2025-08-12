f47b5104a9018a58c9112cb42339cfe5c3ef7a8a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-12 15:21:32 +0330

    Create ShoppingBasketSection component...

---
+++ src/components/shared/ShoppingBaskectCom/ShoppingBasketSection.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '.ShoppingBasketSection.css'
+
+export default function ShoppingBasketSection() {
+  return (
+    <section className='shopping-basket-section'>ShoppingBasketSection</section>
+  )
+}
