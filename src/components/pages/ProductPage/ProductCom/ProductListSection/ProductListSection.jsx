749cc07e314818be867d8effbf2b65282611fbd9
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-13 09:57:27 +0330

    Create ProductListSection component...

---
+++ src/components/pages/ProductPage/ProductCom/ProductListSection/ProductListSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ProductListSection.css";
+
+export default function ProductListSection() {
+  return (
+    <div className="product-list-bg">
+      <section className="product-list-section"></section>
+    </div>
+  );
+}
--- src/components/pages/ProductPage/ProductPage.jsx
+++ src/components/pages/ProductPage/ProductPage.jsx
@@ -4,12 +4,14 @@ import Navbar from "../../shared/Navbar/Navbar";
 import PageHeader from "../../shared/PageHeader/PageHeader";
 import SocialSection from "../../shared/SocialSection/SocialSection";
 import Footer from "../../shared/Footer/Footer";
+import ProductListSection from "./ProductCom/ProductListSection/ProductListSection";
 
 export default function ProductPage() {
   return (
     <>
       <Navbar />
       <PageHeader title={"Shop Details"} />
+      <ProductListSection />
       <SocialSection />
       <Footer />
     </>
