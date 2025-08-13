327508c0c6c093cd988ddea02c0dbc0f034fd169
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-13 09:32:48 +0330

    Add components..

--- src/components/pages/ProductPage/ProductPage.jsx
+++ src/components/pages/ProductPage/ProductPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './ProductPage.css'
+import React from "react";
+import "./ProductPage.css";
+import Navbar from "../../shared/Navbar/Navbar";
+import PageHeader from "../../shared/PageHeader/PageHeader";
+import SocialSection from "../../shared/SocialSection/SocialSection";
+import Footer from "../../shared/Footer/Footer";
 
 export default function ProductPage() {
   return (
     <>
-        
+      <Navbar />
+      <PageHeader title={"Shop Details"} />
+      <SocialSection />
+      <Footer />
     </>
-  )
+  );
 }
