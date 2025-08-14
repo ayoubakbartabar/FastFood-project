5e2ac005e1a5d91af40cf1acd8e2c80ef6ec760a
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-14 10:23:23 +0330

    Update ProductListSection.css , BestSellingData.js : set styles and set new property...

--- src/components/pages/HomePage/HomePageCom/BestSellingSection/BestSellingData.js
+++ src/components/pages/HomePage/HomePageCom/BestSellingSection/BestSellingData.js
@@ -3,6 +3,8 @@ const BestSellingData = [
     id: 1,
     image: "/image/66210efa8e4b4099fd114212_pasta image.png",
     title: "Pasta with Tomatoes and Basil",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 5,
     price: 30.0,
     count: 10,
@@ -14,6 +16,8 @@ const BestSellingData = [
     image:
       "/image/661cbfbcac04478f46ca34b9_78178740_Fresh beef burger isolated -1 2.png",
     title: "Testy Beef Burger",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 5,
     price: 41.0,
     count: 13,
@@ -24,6 +28,8 @@ const BestSellingData = [
     id: 3,
     image: "/image/661cc038dcc14c75443911c3_image.png",
     title: "Spinach Artichoke Spectacle",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 5,
     price: 48.0,
     count: 18,
@@ -34,6 +40,8 @@ const BestSellingData = [
     id: 4,
     image: "/image/661cc01f38e41da22474a52a_fast_food_set 1.png",
     title: "TNC Combo Meals",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 4.5,
     price: 41.0,
     count: 17,
@@ -45,6 +53,8 @@ const BestSellingData = [
     image:
       "/image/661cc047c3a82b8be8890aa0_wepik-export-20240119111930TbUs 1.png",
     title: "Tasty Mint Julep Cocktail",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 3.5,
     price: 75.0,
     count: 20,
@@ -55,6 +65,8 @@ const BestSellingData = [
     id: 6,
     image: "/image/661cc009926e82c8f0e27899_Tasty mint julep cocktail 1.png",
     title: "Lemon Ice Tea",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 5,
     price: 15.0,
     count: 8,
@@ -65,6 +77,8 @@ const BestSellingData = [
     id: 7,
     image: "/image/66210df4c3b3464a6a16c59c_image.png",
     title: "Fried Chicken Legs",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 5,
     price: 20.0,
     count: 4,
@@ -75,6 +89,8 @@ const BestSellingData = [
     id: 8,
     image: "/image/661cbf8e0563fed591d090fd_image-2.png",
     title: "Philly Steak Sandwich",
+    paragraph:
+      "Pizza ipsum dolor meat lovers buffalo. Sausage large wing bell NY olives pan. Spinach pan string pan spinach. Sautéed marinara rib buffalo lasagna pork sausage olives tomatoes NY.",
     star: 4.5,
     price: 35.0,
     count: 7,
