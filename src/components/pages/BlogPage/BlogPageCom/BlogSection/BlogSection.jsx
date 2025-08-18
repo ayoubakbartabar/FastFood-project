060bf591701f5c43765afa46a6f21dc647cb049e
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-18 12:18:46 +0330

    Update BlogSection.jsx : create tags....

--- src/components/pages/BlogPage/BlogPageCom/BlogSection/BlogSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogSection/BlogSection.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import './BlogSection.css'
+import React from "react";
+import "./BlogSection.css";
+import BlogAsideSection from "../BlogAsideSection/BlogAsideSection";
 
 export default function BlogSection() {
   return (
-    <section className='blog-section'></section>
-  )
+    <section className="blog-section">
+      <div className="blog-section-main-wrapper">
+        <div className="blog-aside-section">
+          <BlogAsideSection />
+        </div>
+
+        <div className="blog-section-content"></div>
+      </div>
+    </section>
+  );
 }
