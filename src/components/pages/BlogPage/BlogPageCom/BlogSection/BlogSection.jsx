acfd1f2a340c8091ace42e91562a50ebdf84605d
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-19 12:30:01 +0330

    Update BlogSection.jsx : create tags and set location for share data...

--- src/components/pages/BlogPage/BlogPageCom/BlogSection/BlogSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogSection/BlogSection.jsx
@@ -1,22 +1,38 @@
 import React from "react";
-import "./BlogSection.css";
-import BlogAsideSection from "../BlogAsideSection/BlogAsideSection";
+import { useLocation } from "react-router-dom";
 import Navbar from "../../../../shared/Navbar/Navbar";
 import PageHeader from "../../../../shared/PageHeader/PageHeader";
 import SocialSection from "../../../../shared/SocialSection/SocialSection";
 import Footer from "../../../../shared/Footer/Footer";
-
+import "./BlogSection.css";
+import BlogAsideSection from "../BlogAsideSection/BlogAsideSection";
 export default function BlogSection() {
+  const location = useLocation();
+  const post = location.state?.post;
+
+  if (!post) return <p>Post not found</p>;
+
   return (
     <section className="blog-section">
       <Navbar />
-      <PageHeader title={"blog"} />
+      <PageHeader title={post.title} />
       <div className="blog-section-main-wrapper">
         <div className="blog-aside-section">
           <BlogAsideSection />
         </div>
+        <div className="blog-section-content">
+          <img src={post.image} alt={post.title} className="blog-main-image" />
+          <span className="blog-category">{post.categories}</span>
 
-        <div className="blog-section-content"></div>
+          {post.content.map((block, index) => {
+            if (block.type === "paragraph") {
+              return <p key={index}>{block.text}</p>;
+            } else if (block.type === "title") {
+              return <h2 key={index}>{block.text}</h2>;
+            }
+            return null;
+          })}
+        </div>
       </div>
       <SocialSection />
       <Footer />
