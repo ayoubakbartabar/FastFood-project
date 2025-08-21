99c7ca1aaad7742c4380a771c0f02cde31636bc3
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-21 11:12:37 +0330

    Update BlogCategoriesSection.jsx : add components...

--- src/components/pages/BlogPage/BlogPageCom/BlogCategoriesSection/BlogCategoriesSection.jsx
+++ src/components/pages/BlogPage/BlogPageCom/BlogCategoriesSection/BlogCategoriesSection.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./BlogCategoriesSection.css ";
+import Navbar from "../../../../shared/Navbar/Navbar";
+import PageHeader from "../../../../shared/PageHeader/PageHeader";
+import SocialSection from "../../../../shared/SocialSection/SocialSection";
+import Footer from "../../../../shared/Footer/Footer";
 
 export default function BlogCategoriesSection() {
   return (
     <div className="blog-categories-bg">
+      <Navbar />
+      <PageHeader title={"enter categories name "} />
       <section className="blog-categories-section"></section>
+      <SocialSection />
+      <Footer />
     </div>
   );
 }
