41e3a6dffae06a204fea391de3c79fb01b2386e8
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-21 11:08:40 +0330

    Create BlogCategoriesSection component...

---
+++ src/components/pages/BlogPage/BlogPageCom/BlogCategoriesSection/BlogCategoriesSection.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BlogCategoriesSection.css ";
+
+export default function BlogCategoriesSection() {
+  return (
+    <div className="blog-categories-bg">
+      <section className="blog-categories-section"></section>
+    </div>
+  );
+}
