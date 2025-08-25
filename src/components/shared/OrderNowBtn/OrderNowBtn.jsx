a7b9933f3d39d26c6c7250446c513c8b2a923103
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-25 08:57:20 +0330

    Update OrderNowBtn.jsx : add navigate...

--- src/components/shared/OrderNowBtn/OrderNowBtn.jsx
+++ src/components/shared/OrderNowBtn/OrderNowBtn.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./OrderNowBtn.css";
 import { MdArrowRightAlt } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 export default function OrderNowBtn({ variant = "desktop" }) {
+  const navigate = useNavigate();
+
   const className = `order-now-btn ${
     variant === "mobile" ? "mobile-order-now" : "desktop-order-now"
   }`;
 
   return (
-    <button className={className}>
+    <button className={className} onClick={() => navigate("/menu")}>
       order now
       <MdArrowRightAlt />
     </button>
