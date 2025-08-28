baf27e7322b023631b9b64adb1b72fc9bf66cbcb
Ayoub Akbartabar <ayoubakbartabar1887@gmail.com>
2025-08-28 08:31:49 +0330

    Update NotFoundPage.jsx : add components...

--- src/components/pages/NotFoundPage/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import './NotFoundPage.css'
+import React from "react";
+import "./NotFoundPage.css";
+import Navbar from "../../shared/Navbar/Navbar";
+import PageHeader from "../../shared/PageHeader/PageHeader";
+import SocialSection from "../../shared/SocialSection/SocialSection";
+import Footer from "../../shared/Footer/Footer";
 
 export default function NotFoundPage() {
   return (
     <>
-      NotFoundPage
+      <Navbar />
+      <PageHeader title={"404"} />
+      <SocialSection />
+      <Footer />
     </>
-  )
+  );
 }
